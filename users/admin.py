0d3b678a18af3c54495cc44cb544ff1122ed2158
Daniil <jeiromkrizhanovskiyd@gmail.com>
2025-06-10 15:19:34 +0500

    Add doc-string in UserAdmin

--- users/admin.py
+++ users/admin.py
@@ -4,6 +4,9 @@ from users.models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
+    """
+    Админ-панель для отображения пользователей. Редактирования\удаления\добавления.
+    """
     list_display = ("email", "first_name", "last_name", "is_active")
     search_fields = ("email", "first_name", "last_name")
     list_filter = ("is_active",)
